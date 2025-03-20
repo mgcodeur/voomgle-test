9bc29aef91bd99a7c6450c5d7d34b929c2e0bdc5
mgcodeur <mgcodeur@gmail.com>
2025-03-20 20:45:22 +0300

    initialisation du projet

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+const email = ref('maeldev3@gmail.com');
+const password = ref('password');
+
+
+const login = async () => {
+  const { data, error } = await useFetch('https://api.staging.voomgle.com/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: {
+        email: email.value,
+        password: password.value
+      }
+    });
+
+    console.log(data.value);
+};
+
+login();
+</script>
